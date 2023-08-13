2f2de39d8c84318d8d18d8ad3f90b82f64760d59
zerno7736 <141718405+zerno7736@users.noreply.github.com>
2023-08-13 18:13:44 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,28 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
 
+
+link = "https://suninjuly.github.io/math.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+input1 = browser.find_element(By.XPATH, "//input[@id='answer']")
+input1.send_keys(y)
+time.sleep(2)
+
+input2 = browser.find_element(By.XPATH, "//input[@id='robotCheckbox']")
+input2.click()
+time.sleep(2)
+
+input3 = browser.find_element(By.XPATH, "//input[@id='robotsRule']")
+input3.click()
+time.sleep(2)
+
+button = browser.find_element(By.XPATH, "//button[@type='submit']")
+button.click()
+
+# закрываем браузер после всех манипуляций
+time.sleep(10)
+browser.quit()
